973ed3d3948ce79e600a4878b3fbb0cd5f39df7e
Théo <t.lecoublet@gmail.com>
2022-09-04 17:39:19 +0200

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 // _app.tsx
-
+import '../styles/globals.css'
+import 'tailwindcss/tailwind.css';
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 
--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function signin() {
     const router = useRouter();
-  return (
-    <div>signin
+  return (/*
+    <div>
+      <h1 classNameName="text-3xl font-bold underline">
+        signin
+      </h1>
         <ul>
             <li onClick={() =>
                 signIn("github", {
@@ -20,7 +24,84 @@ export default function signin() {
                 GOOGLE
             </li>
         </ul>
+        
+    </div>*/
+
+
+    <div className="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
+        <Link href="/">
+        <svg className="cursor-pointer w-11 h-11 p-2 rounded-full hover:bg-slate-100" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M224 480h640a32 32 0 110 64H224a32 32 0 010-64z"/><path fill="currentColor" d="M237.248 512l265.408 265.344a32 32 0 01-45.312 45.312l-288-288a32 32 0 010-45.312l288-288a32 32 0 1145.312 45.312L237.248 512z"/></svg>
+
+        </Link>
+        <h1 className="w-full text-4xl font-medium text-center">Login</h1>
+        <p className="text-slate-500"></p>
+        <div className="my-5">
+            <button 
+                onClick={() =>
+                    signIn("google", {
+                    callbackUrl: router.query.callbackUrl.toString(),
+                    })} 
+                className="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
+                
+                <svg className="mx-4 w-6 h-6" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Google</title><path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"/></svg>
+                Login with Google
+            </button> 
+            <button onClick={() =>
+                signIn("github", {
+                  callbackUrl: router.query.callbackUrl.toString(),
+                })} className="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
+
+                <svg className="mx-4 w-6 h-6" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
+                Login with Github
+            </button> 
+        </div>
+        <div className="w-full flex items-center justify-between py-5">
+                         <hr className="w-full bg-gray-400"/>
+                         <p className="text-base font-medium leading-4 px-2.5 text-gray-400">OR</p>
+                         <hr className="w-full bg-gray-400  "/>
+        </div>
+        <form action="" className="my-10">
+            <div className="flex flex-col space-y-5">
+                <label htmlFor="email">
+                    <p className="font-medium text-slate-700 pb-2">Email address</p>
+                    <input id="email" name="email" type="email" className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Enter email address"/>
+                </label>
+                <label htmlFor="password">
+                    <p className="font-medium text-slate-700 pb-2">Password</p>
+                    <input id="password" name="password" type="password" className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Enter your password"/>
+                </label>
+                {/* <div className="flex flex-row justify-between">
+                    <div>
+                        <label htmlFor="remember" className="">
+                            <input type="checkbox" id="remember" className="w-4 h-4 border-slate-200 focus:bg-indigo-600"/>
+                            Remember me
+                        </label>
+                    </div>
+                    <div>
+                        <a href="#" className="font-medium text-indigo-600">Forgot Password?</a>
+                    </div>
+                </div> */}
+                <button className="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                      </svg>
+                      <span>Login</span>
+                </button>
+                <Link href={`signup${
+                        router.query.callbackUrl
+                          ? `?callbackUrl=${router.query.callbackUrl}`
+                          : ""
+                      }`}>
+                    <p className="text-center">Not registered yet? <a href="#" className="text-indigo-600 font-medium inline-flex space-x-1 items-center"><span>Register now </span><span><svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                    </svg></span></a>
+                    </p>
+                </Link>
+                
+            </div>
+        </form>
     </div>
+    
   )
 }
 
